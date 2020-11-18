c7e2d67d31d5c04de73d47e67495659f405a5352
Chris Patrick <atomize101@gmail.com>
2020-11-18 14:26:39 -0500

    Setting up bootstrap and front end structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
-
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const token = 'sk_dc95d97ef48e41858d36653e75cbc720';
 
 const App = () => {
-	const url = 'https://cloud.iexapis.com/stable/stock/wmt/intraday-prices?token=' + token;
-	const data = fetch(url)
-		.then((response) => response.json())
-		.then((data) => console.log(data));
-	return <div className="App">Hello!</div>;
+	const url = 'https://cloud.iexapis.com/stable/stock/wmt/intraday-prices?chartLast=1&token=' + token;
+	var prices;
+	//const data = fetch(url)
+	//	.then((response) => response.json())
+	//	.then((data) => (prices = data));
+	return (
+		<div className="App">
+			<div className="container">
+				<table className="table mt-5">
+					<thead>
+						<tr>
+							<th>Ticker</th>
+							<th>Ticker</th>
+							<th>Ticker</th>
+							<th>Ticker</th>
+						</tr>
+					</thead>
+				</table>
+			</div>
+		</div>
+	);
 };
 
 export default App;
