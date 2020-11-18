8345bb4afe7927350f6aa6cda596722382700fae
Chris Patrick <atomize101@gmail.com>
2020-11-18 13:23:00 -0500

    First a simple graph

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import '../node_modules/react-vis/dist/style.css';
+import { XYPlot, LineSeries } from 'react-vis';
 
 import './App.css';
 
 const App = () => {
+	const data = [
+		{ x: 0, y: 8 },
+		{ x: 1, y: 5 },
+		{ x: 2, y: 4 },
+		{ x: 3, y: 9 },
+		{ x: 4, y: 1 },
+		{ x: 5, y: 7 },
+		{ x: 6, y: 6 },
+		{ x: 7, y: 3 },
+		{ x: 8, y: 2 },
+		{ x: 9, y: 0 },
+	];
+
 	return (
-		<div>
-			<h1>Stock Ticker App</h1>
+		<div className="App">
+			<XYPlot height={300} width={300}>
+				<LineSeries data={data} />
+			</XYPlot>
 		</div>
 	);
 };
